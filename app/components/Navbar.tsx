ba19d1aa5a7e69dcfacc74dd5bd31f539a2ede26
Sajana <svwijesinghe97@gmail.com>
2025-09-23 20:23:29 -0500

    Mobile Responsive Changes II

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import type { User } from "@firebase/auth";
-import { Link, useLocation, useParams } from "react-router";
+import { Link, useNavigate } from "react-router";
 import { authFunctions } from "~/lib/firebase";
+import { NavbarLinks } from "../../constants/navbar";
 
 const Navbar = ({ user }: { user: User | null }) => {
+  const navigate = useNavigate();
   const { logoutFunction } = authFunctions();
-  const { id } = useParams<{ id: string }>();
-  const currentUrl = useLocation();
 
-  return (
-    <nav
+    return (
+        <nav
       className={
-        "w-full bg-gray-50 shadow-md border-b border-gray-200 flex items-center justify-between py-4 px-8"
+        "w-full bg-gray-50 shadow-md border-b border-gray-200 flex items-center justify-between py-4 px-8 sticky top-0 z-50"
       }
     >
       <Link to={"/"} className={"flex flex-row items-center gap-4"}>
@@ -21,64 +21,40 @@ const Navbar = ({ user }: { user: User | null }) => {
         </h3>
       </Link>
 
-      <div className={`${user ? "visible" : "hidden"} flex flex-row gap-5`}>
-        <Link
-          to={"/create-blog-post"}
-          className={
-            "bg-gray-900 text-white px-4 py-2 rounded hover:bg-gray-700"
-          }
-        >
-          Create Post
-        </Link>
+      <div className={"hidden md:flex md:flex-row items-center gap-4"}>
+        {NavbarLinks.map((ele, i) => (
+          <div key={i}>
+            <Link
+              to={ele.url}
+              className={
+                "text-lg font-semibold text-blue-400 hover:text-blue-700"
+              }
+            >
+              {ele.title}
+            </Link>
+          </div>
+        ))}
 
-        <button
-          onClick={() => logoutFunction()}
-          className={
-            "bg-gray-600 text-white px-4 py-2 rounded cursor-pointer hover:bg-gray-700 focus:outline-none"
-          }
-        >
-          Logout
-        </button>
-      </div>
-
-      <div className={"flex flex-row gap-5"}>
-        <div
-          className={`${id || !currentUrl.pathname.startsWith("/about") ? "visible" : "hidden"}`}
-        >
-          <Link
-            to={"/about"}
+        <div className={`${user ? "visible" : "hidden"}`}>
+          <button
             className={
-              "bg-blue-400 text-white px-4 py-2 rounded hover:bg-blue-700"
+              "bg-blue-400 text-white px-4 py-2 rounded cursor-pointer hover:bg-blue-700 focus:outline-none mr-4"
+            }
+            onClick={() => {
+              navigate("/create-blog-post");
+            }}
+          >
+            Create Post
+          </button>
+
+          <button
+            onClick={() => logoutFunction()}
+            className={
+              "bg-blue-400 text-white px-4 py-2 rounded cursor-pointer hover:bg-blue-700 focus:outline-none"
             }
           >
-            About
-          </Link>
-        </div>
-
-        <div
-          className={`${id || !currentUrl.pathname.startsWith("/blog") ? "visible" : "hidden"}`}
-        >
-          <Link
-            to={"/blog"}
-            className={
-              "bg-blue-400 text-white px-4 py-2 rounded hover:bg-blue-700"
-            }
-          >
-            Blog
-          </Link>
-        </div>
-
-        <div
-          className={`${id || !currentUrl.pathname.startsWith("/lawsuit") || currentUrl.pathname.endsWith("documents") ? "visible" : "hidden"}`}
-        >
-          <Link
-            to={"/lawsuit"}
-            className={
-              "bg-blue-400 text-white px-4 py-2 rounded hover:bg-blue-700"
-            }
-          >
-            Lawsuit
-          </Link>
+            Logout
+          </button>
         </div>
       </div>
     </nav>
---
+++ constants/navbar/index.ts
@@ -0,0 +1,14 @@
+export const NavbarLinks = [
+  {
+    title: "About",
+    url: "/about",
+  },
+  {
+    title: "Blog",
+    url: "/blog",
+  },
+  {
+    title: "Lawsuit",
+    url: "/lawsuit",
+  },
+];
