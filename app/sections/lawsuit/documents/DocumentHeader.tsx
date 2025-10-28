6b8af3a0320f526e70c95773edc3c7a82b97556f
Sajana <svwijesinghe97@gmail.com>
2025-10-27 22:08:22 -0400

    10/27/25 - Customer Change Request I

--- app/sections/lawsuit/documents/DocumentHeader.tsx
+++ app/sections/lawsuit/documents/DocumentHeader.tsx
@@ -11,10 +11,20 @@ const DocumentHeader = () => {
         Legal Documents
       </h1>
       <p className={"text-lg text-gray-700 w-full"}>
-        Explore all legal records related to the Mead Memorial Chapel case.
-        Documents are carefully categorized to reflect the structure and claims
-        of the ongoing lawsuit, making it easier to locate filings, exhibits,
-        and correspondence by topic. Each file can be{" "}
+        Explore all legal records related to the appeal to the Vermont Supreme
+        Court. Prior motions and documents from the Vermont Superior Court can
+        be found at the{" "}
+        <span>
+          <a
+            href={"https://meadmemorialchapel.com/"}
+            className={"text-blue-600 underline"}
+          >
+            original site
+          </a>
+        </span>
+        . Documents are carefully categorized to reflect the structure and
+        claims of the ongoing lawsuit, making it easier to locate filings,
+        exhibits, and correspondence by topic. Each file can be{" "}
         <b>viewed online or downloaded</b> for offline review.
       </p>
     </section>
