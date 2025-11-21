268993266cb847b28cfdfdb5da99cf4f4bd6f482
Sajana <svwijesinghe97@gmail.com>
2025-11-21 13:42:30 -0500

    11/21/25 - Max Width Undo

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -31,7 +31,7 @@ export default function Home() {
   }, []);
 
   return (
-    <main className={"font-serif container"}>
+    <main className={"font-serif"}>
       <section
         className={
           "md:px-25 px-5 relative bg-gray-900 text-white h-[50vh] flex flex-col justify-center items-start text-left"
